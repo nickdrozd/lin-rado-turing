0cf06132ae03aa86b5a9b43b80949dd3a9fea937
Boyd Johnson <johnson.boyd@gmail.com>
2021-02-11 16:52:49 +0000

    Changes to Tape

--- src/tape.rs
+++ src/tape.rs
@@ -37,18 +37,19 @@ impl<Sym: Symbol> Tape<Sym> {
     }
 
     pub fn iter_between<'a>(&'a self, first: i64, last: i64) -> impl Iterator<Item = Sym> + 'a {
-        assert!(first <= last);
-        (first..last).map(move |ref pos| self.0.get(pos).copied().unwrap_or_else(Sym::zero))
+        (self.1.min(first)..self.2.max(last))
+            .filter(move |pos| pos < &last && pos >= &first)
+            .map(move |ref pos| self.0.get(pos).copied().unwrap_or_else(Sym::zero))
     }
 
     pub fn iter_to<'a>(&'a self, to: i64) -> impl Iterator<Item = Sym> + 'a {
-        (self.1..self.2)
+        (self.1..self.2.max(to))
             .filter(move |pos| pos < &to)
             .map(move |ref pos| self.0.get(pos).copied().unwrap_or_else(Sym::zero))
     }
 
     pub fn iter_from<'a>(&'a self, from: i64) -> impl Iterator<Item = Sym> + 'a {
-        (self.1..self.2)
+        (self.1.min(from)..self.2)
             .filter(move |pos| &from <= pos)
             .map(move |ref pos| self.0.get(pos).copied().unwrap_or_else(Sym::zero))
     }
@@ -65,7 +66,7 @@ impl<Sym: Symbol> Tape<Sym> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::types::TwoSymbol;
+    use crate::types::{ThreeSymbol, TwoState, TwoSymbol};
 
     #[test]
     fn test_write_iter() {
@@ -121,5 +122,42 @@ mod tests {
         tape.write(2, TwoSymbol::One);
 
         assert_eq!(tape.size(), 4);
+    }
+
+    #[test]
+    fn test_iter_between() {
+        let mut tape = Tape::default();
+
+        tape.write(-2, ThreeSymbol::One);
+
+        tape.write(-1, ThreeSymbol::One);
+
+        tape.write(0, ThreeSymbol::Two);
+
+        tape.write(1, ThreeSymbol::Two);
+
+        tape.write(2, ThreeSymbol::One);
+
+        tape.write(3, ThreeSymbol::One);
+
+        assert_eq!(
+            tape.iter_between(-2, 1).collect::<Vec<_>>(),
+            vec![ThreeSymbol::One, ThreeSymbol::One, ThreeSymbol::Two]
+        );
+
+        assert_eq!(
+            tape.iter_between(-1, 2).collect::<Vec<_>>(),
+            vec![ThreeSymbol::One, ThreeSymbol::Two, ThreeSymbol::Two]
+        );
+
+        assert_eq!(
+            tape.iter_between(-5, -1).collect::<Vec<_>>(),
+            vec![
+                ThreeSymbol::Zero,
+                ThreeSymbol::Zero,
+                ThreeSymbol::Zero,
+                ThreeSymbol::One
+            ]
+        );
     }
 }
